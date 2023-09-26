617037c0809c90e3ec9878fae83220752c31d0c4
Brian Barr <bbarr0101@gmail.com>
2023-09-26 15:26:12 -0700

    adds method to add connection to connection set

--- peer.py
+++ peer.py
@@ -1,15 +1,23 @@
 import argparse 
 import socket
+from connection import *
 from utils import * 
 
 parser = argparse.ArgumentParser(description='enter the port number for you\'re peers to connect to')
 parser.add_argument('--port', '-p', type=int)
 
 args = parser.parse_args()
-connections = {}
+global con_id
+con_id = 1
+connections = set() 
 
-
-
+def add_conn():
+    ip = input("enter target IP address: ")
+    port = input("enter target port: ")
+    sock_tup = (ip, port)
+    global con_id
+    connections.add(connection(con_id, sock_tup))
+    con_id +=1 
 
 if __name__ == "__main__":
     port = args.port
@@ -23,10 +31,11 @@ if __name__ == "__main__":
             case 'help':
                 prompt()
             case 'connect':
-                #make_conn()
+                add_conn()
                 print('here are the connections')
             case 'exit':
                 exit()
             case 'list':
                 print('here are the connections')
+                print(connections)
 

7032cccc42bb478e48c1eb73475443a02ac23268
Brian Barr <bbarr0101@gmail.com>
2023-09-26 14:40:44 -0700

    adds file for the connection class

---
+++ connection.py
@@ -0,0 +1,9 @@
+#documnent for creating connection objects 
+
+class connection():
+    def __init__(self, id, sock_tup):
+        self.id = id
+        self.sock = sock_tup
+
+    def __repr__(self) -> str:
+        return f"id: {self.id} \n address: {self.sock[0]} \n port: {self.sock[1]} " 

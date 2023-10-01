2762020f568f7ab61728fbdc6a54a4184af37840
Brian Barr <bbarr0101@gmail.com>
2023-10-01 14:34:04 -0700

    removes client.py and server.py

--- client.py
+++
@@ -1,8 +0,0 @@
-import socket
-import utils
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    #open connection to the target addr
-    s.connect((utils.get_ip(), 8000))
-    msg = s.recv(1024)
-    print(msg.decode())
